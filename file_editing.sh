ec00a85cc9f79f1ab4bafd931b8bfc9bb79e9be2
anushayell <anusha2003.y@gmail.com>
2024-12-01 17:03:32 +0530

    Update file_editing.sh

--- file_editing.sh
+++ file_editing.sh
@@ -4,15 +4,13 @@ working_dir=$(pwd)
 new_dir="practice"
 
 mkdir  $working_dir/$new_dir
-
 cd  $working_dir/$new_dir
 
 n=1
 while [ $n -le 10 ]; do
- touch files${n}.txt
-echo "welcome to bash world ${n} " > files${n}.txt
-((n++))
+  touch files${n}.txt
+  echo "welcome to bash world ${n} " > files${n}.txt
+  ((n++))
 done
-
 
 echo "created  the files with data"
